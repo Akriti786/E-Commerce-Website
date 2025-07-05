879f47e22dac2e4daacecfe365f599bd1b7122c7
Akriti786 <akriti786shukla@gmail.com>
2025-07-05 14:26:22 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,99 @@
-import React from 'react'
-import {MdLocalShipping} from 'react-icons/md'
+import React from 'react';
+import { MdLocalShipping } from 'react-icons/md';
+import './Navbar.css';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FiLogIn } from 'react-icons/fi';
+import { useAuth0 } from "@auth0/auth0-react";
+import { CiLogout, CiUser } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
   return (
     <div className='header'>
+
+
       <div className='top_header'>
         <div className='icon'>
-            <MdLocalShipping />
+          <MdLocalShipping />
+        </div>
+        <div className='info'>
+          <p>Free Shipping on Orders Over $500</p>
+        </div>
+      </div>
+
+      <div className='mid_header'>
+        <div className='logo'>
+          <img src="comlogo.jpeg" alt="logo" />
+        </div>
+
+        <div className='mid_right'>
+          <div className='search_box'>
+            <input type="text" placeholder='search' />
+            <button><AiOutlineSearch /></button>
+          </div>
+
+          {
+            isAuthenticated ?
+              //Logout button
+              <button className='logout_btn' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                <CiLogout className='login_icon' />
+                <span>Logout</span>
+              </button>
+              :
+              //Login button
+              <button className='login_btn' onClick={() => loginWithRedirect()}>
+                <FiLogIn className='login_icon' />
+                <span>Login</span>
+              </button>
+          }
+        </div>
+      </div>
+
+
+      <div className='last_header'>
+        <div className='user_profile'>
+          {
+            //User profile will be shown here 
+            isAuthenticated ?
+              <>
+                <div className='icon'>
+                  <CiUser />
+                </div>
+                <div className='info'>
+                  <h2>{user.name}</h2>
+                  <p>{user.email}</p>
+                </div>
+              </>
+              :
+              <>
+                <div className='icon'>
+                  <CiUser />
+                </div>
+                <div className='info'>
+                  <p>Please Login</p>
+                </div>
+              </>
+          }
+
+        </div>
+        <div className='nav'>
+          <ul>
+            <li><Link to='/' className='link'>Home</Link></li>
+            <li><Link to='/shop' className='link'>Shop</Link></li>
+            <li><Link to='/collection' className='link'>Collection</Link></li>
+            <li><Link to='/about' className='link'>About</Link></li>
+            <li><Link to='/contact' className='link'>Contact</Link></li>
+          </ul>
+        </div>
+        <div className='offer'>
+          <p>Flat 10% over all phone</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Auth0Provider
+    domain="dev-zqtb5u87bgfrjccr.us.auth0.com"
+    clientId="x8iF9ZLeiU3vaLKuwjSOpUzHue7htlYD"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+     <App />
+  </Auth0Provider>
 )
