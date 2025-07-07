009141e28a6c19406c87b996444c131f96ccd225
Akriti786 <akriti786shukla@gmail.com>
2025-07-07 17:01:25 +0530

    upppp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
+import HomeProducts from './HomeProducts';
+import { AiFillEye } from 'react-icons/ai';
+import { AiFillHeart } from 'react-icons/ai';
 
 const Home = () => {
-  return (
-    <div className='home'>
-        <div className='top_banner'>
 
+  const [trendingProduct, setTrendingProduct] = useState(HomeProducts);
+
+  return (
+    <>
+      <div className='home'>
+        <div className='top_banner'>
+          <div className='content'>
+            <h3>Samsung Galaxy S8</h3>
+            <h2>Samsung Phone</h2>
+            <p>30% off on your first order</p>
+            <Link to='/shop' className='link'>Shop Now</Link>
+          </div>
         </div>
-    </div>
+        <div className='trending'>
+          <div className='container'>
+            <div className='left_box'>
+              <div className='headers'>
+                <div className='heading'>
+                  <h2>trending product</h2>
+                </div>
+                <div className='cate'>
+                  <h3>New</h3>
+                  <h3>Featured</h3>
+                  <h3>Top Selling</h3>
+                </div>
+              </div>
+              <div className='products'>
+                <div className='container'>
+                  {
+                    trendingProduct.map((curElm) => {
+                      return(
+                        <>
+                          <div className='box'>
+                            <div className='img_box'>
+                              <img src={curElm.image} alt={curElm.Name}></img>
+                              <div className='icon'>
+                                <div className='icon_box'>
+                                  <AiFillEye />
+                                </div>
+                                  <div className='icon_box'>
+                                  <AiFillHeart />
+                                </div>
+                              </div>
+                            </div>
+                            <div className='info'>
+                              <h3>{curElm.Name}</h3>
+                              <p>${curElm.price}</p>
+                              <button className='btn'>Add to cart</button>
+                            </div>
+                          </div>
+                        </>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+            <div className='right_box'></div>
+          </div>
+        </div>
+      </div>
+    </>
+
   )
 }
 
---
+++ src/components/HomeProducts.jsx
@@ -0,0 +1,71 @@
+const HomeProducts = 
+[
+    {
+        id:1,
+        Name:'Bluetooth Speaker',
+        price:'26',
+        image:'/image/product1.webp',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:2,
+        Name:'Earbuds',
+        price:'26',
+        image:'/image/product2.avif',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:3,
+        Name:'Television',
+        price:'26',
+        image:'/image/product3.webp',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:4,
+        Name:'Smart Watch',
+        price:'26',
+        image:'/image/product4.avif',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:5,
+        Name:'product5',
+        price:'26',
+        image:'/image/product5.jpg',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:6,
+        Name:'product6',
+        price:'26',
+        image:'/image/product6.webp',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:7,
+        Name:'product7',
+        price:'26',
+        image:'/image/product7.webp',
+        cat:'phone',
+        type:'new',
+    },
+     {
+        id:8,
+        Name:'product8',
+        price:'26',
+        image:'/image/product8.webp',
+        cat:'phone',
+        type:'new',
+    }
+
+]
+
+
+export default HomeProducts;
